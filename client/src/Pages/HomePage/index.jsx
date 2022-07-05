89dd8416523150fc6653d9ef3b52c6a81b1cc08f
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-07-05 09:16:26 -0300

    fix: HomePage - botoes signin e signup

--- client/src/Pages/HomePage/index.jsx
+++ client/src/Pages/HomePage/index.jsx
@@ -1,38 +1,77 @@
 import React from 'react';
-import SignIn from '../SignIn';
-import SignUp from '../SignUp';
+import Button from '@mui/material/Button';
 import './style.css';
+import Box from '@mui/material/Box';
+import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  const handleNavigate = (route) => {
+    navigate(`/${route}`);
+  };
+
   return (
-    <section>
-      <h1>Bem vindos a Ebytr Schedule!</h1>
-      <p>
-        Essa aplicação tem por objetivo auxiliá-los na organização de suas
-        tarefas diárias.
-      </p>
-      <p>
-        Nela você será capaz de: Cadstrar, Atualizar, Consultar e Excluir suas
-        tarefas.
-        {/* , bem como alterar o status de cada uma delas e filtrar por:
-        ordem alfabetica, data de criacao e status; */}
-      </p>
-      <p>
-        Se este for o seu primeiro acesso, será necessário o cadastramento,
-        preencha os dados em SIGNUP e coloque seus dados.
-      </p>
-      <p>
-        Caso contrário, basta inserir os dados de login em SIGNIN e realizar.
-      </p>
+    <>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          border: '1px solid #cfcfcf',
+          borderRadius: '5px',
+          '&:hover': {
+            border: '1px solid black',
+          },
+        }}
+      >
+        <h1>Bem vindos a Ebytr Schedule!</h1>
+        <p>
+          Essa aplicação tem por objetivo auxiliá-los na organização de suas
+          tarefas diárias.
+        </p>
+        <p>
+          Nela você será capaz de: Cadstrar, Atualizar, Consultar e Excluir suas
+          tarefas.
+          {/* , bem como alterar o status de cada uma delas e filtrar por:
+    ordem alfabetica, data de criacao e status; */}
+        </p>
+        <p>
+          Se este for o seu primeiro acesso, será necessário o cadastramento
+          Clique em SIGNUP e preencha seus dados.
+        </p>
+        <p>
+          Caso contrário, basta clicar em SIGNIN e inserir seu email e senha.
+        </p>
+      </Box>
       <section className='container-home-page'>
         <div className='home-page-register'>
-          <SignIn />
+          <Button
+            role='signin'
+            type='submit'
+            fullWidth
+            variant='contained'
+            sx={{ mt: 6, mb: 2 }}
+            onClick={() => handleNavigate('signin')}
+          >
+            SIGNIN
+          </Button>
         </div>
         <div className='home-page-register'>
-          <SignUp />
+          <Button
+            role='signup'
+            type='submit'
+            fullWidth
+            variant='contained'
+            sx={{ mt: 6, mb: 3 }}
+            onClick={() => handleNavigate('signup')}
+          >
+            SIGNUP
+          </Button>
         </div>
       </section>
-    </section>
+    </>
   );
 }
 
